f9b6d65646e35aec9301f730d3f5f24cdaade9fa
Manchien <manchien1105@gmail.com>
2024-04-19 01:59:08 +0800

    增加登出按鈕
    增加了一點create_event.html ，9-13行，之後有home(?再移過去

--- taskmanagement/tasks/auth_views.py
+++ taskmanagement/tasks/auth_views.py
@@ -28,4 +28,18 @@ def user_login(request):
 
 def user_logout(request):
     logout(request)
-    return redirect('/tasks')  # 修改為登出後要跳轉的頁面，比如首頁
+    return redirect('/login')  # 修改為登出後要跳轉的頁面，比如首頁
+
+def task_view(request):
+    username = request.user.username  # 獲取當前登錄用戶的用戶名
+    return render(request, 'create_event.html', {'username': username})
+
+# @login_required  # 添加登录验证装饰器，确保只有登录用户才能访问该视图
+def my_view(request):
+    if request.user.is_authenticated:  # 检查用户是否已经登录
+        # 用户已登录，可以执行相应的操作，如获取用户名
+        username = request.user.username
+        return render(request, 'create_event.html', {'username': username})
+    else:
+        # 用户未登录，可以返回未登录提示或者重定向到登录页面
+        return HttpResponse("請先登錄")
